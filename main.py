39fb019d4e129189fd3fb0a580138b5deae6fde8
valdeortiz <valdeortiz15@gmail.com>
2020-02-25 20:46:23 -0300

    WTForm and bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask, request,make_response, redirect, render_template
-
+from flask import Flask, flash, url_for,request,make_response, session, redirect, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'super SECRETO'
 
 todos = ['comprar cafe', 'comprar', 'enviar producto']
+
+class LoginFrom(FlaskForm):
+	"""formulario de login"""
+	username = StringField('nombre de usuario', validators=[DataRequired()])
+	password = PasswordField('password', validators=[DataRequired()])
+	submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -14,16 +26,27 @@ def server_error(error):
 
 @app.route("/")
 def index():
-	ip_user = request.remote_addr
+	user_ip = request.remote_addr
 	response = make_response(redirect("/hello"))
-	response.set_cookie("ip_user", ip_user)
+	session['user_ip'] = user_ip
 	return response
 
-@app.route("/hello")
+@app.route("/hello", methods=['GET', 'POST'])
 def hello():
-	user_ip = request.cookies.get("ip_user")
+	user_ip = session.get("user_ip")
+	login_form = LoginFrom()
+	username = session.get('username')
 	context = {
 		"user_ip": user_ip,
 		"todos": todos,
+		"login_form": login_form
 	}
+	if login_form.validate_on_submit():
+		username = login_form.username.data
+		session['username'] = username
+
+		flash('nombre de usuario registrado con exito')
+
+		return redirect(url_for('index'))
+
 	return render_template("hello.html", **context)
