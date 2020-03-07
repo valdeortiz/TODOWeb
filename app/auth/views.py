f2103dc30e68e9dd09ba53a8c42a8005ca73b380
valdeortiz <valdeortiz15@gmail.com>
2020-03-07 19:28:58 -0300

    log in - log out implemented

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,11 @@
 from flask import render_template, flash, session, redirect, url_for
+from flask_login import login_user, login_required, logout_user
+
 from app.forms import LoginForm
 
 from . import auth
+from app.firestone_service import get_user
+from app.models import UserData, UserModel
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,10 +16,34 @@ def login():
 	
 	if login_form.validate_on_submit():
 		username = login_form.username.data
-		session['username'] = username
+		password = login_form.password.data
 
-		flash('nombre de usuario registrado con exito')
+		user_doc = get_user(username)
+
+		if user_doc.to_dict() is not None:
+			password_from_db = user_doc.to_dict()['password']
+
+			if password == password_from_db:
+				user_data = UserData(username, password)
+				user = UserModel(user_data)
+
+				login_user(user)
+				flash('Bienvenido de nuevo')
+
+				redirect(url_for('hello'))
+			else:
+				flash('La informacion no coincide')
+		else:
+			flash('El usuario no existe')
+
 
 		return redirect(url_for('index'))
 
 	return render_template('login.html', **context)
+
+@auth.route('logout')
+@login_required
+def logout():
+	logout_user()
+	flash('Regresa printo')
+	return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, flash, url_for,request,make_response, session, redirect, render_template
 from flask_bootstrap import Bootstrap
+from flask_login import login_required, current_user
 import unittest
+
 
 from app import create_app
 from app.forms import LoginForm
 from app.firestone_service import get_users, get_todos
+
 
 app = create_app()
 
@@ -29,9 +32,11 @@ def index():
 	return response
 
 @app.route("/hello", methods=['GET'])
+@login_required
 def hello():
 	user_ip = session.get("user_ip")
-	username = session.get('username')
+	username = current_user.id
+	
 	context = {
 		"user_ip": user_ip,
 		"todos": get_todos(user_id=username),
